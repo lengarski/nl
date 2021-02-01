c55903c35706c5d94f33c605dff903ae02d8bc03
lengarski <lengarski@gmail.com>
2021-02-01 18:04:08 +0200

    Create Web

---
+++ NLPostCodesWeb/hello.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  
+    $( "#startingPoint" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax( {
+          url: "http://192.168.0.105:8087/api_v1/postcode?postcode=",
+          
+          postcode: request.term,
+          
+          success: function( postcode ) {
+            response( postcode );
+          }
+        } );
+      },
+      minLength: 2,
+      select: function( event, ui ) {
+        console.log( "Selected: " + ui.item.value  );
+      }
+    } );
+    
+     $( "#destinationPoint" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax( {
+          url: "http://192.168.0.105:8087/api_v1/postcode?postcode=",
+          
+          postcode: request.term,
+          
+          success: function( postcode ) {
+            response( postcode );
+          }
+        } );
+      },
+      minLength: 2,
+      select: function( event, ui ) {
+        console.log( "Selected: " + ui.item.value  );
+      }
+    } );
+
+    
+    $("#searchButton").click(function(){	
+		search();
+	});
+    
+    function search(){
+		console.log("search");
+		
+		var start = $("#startingPoint").val();
+		var dest = $("#destinationPoint").val();
+		
+		
+		$.ajax({
+			url: "http://localhost:8087/api_v1/distance?startPostCode="+start+"&destinationPostCode="+dest
+		}).then(function(data) {
+			console.log("asdf");
+			console.log(data);
+       
+			$('#distanceLabel').text("Distance beetween ").append(start) . append (" and ") .append(dest) . append( " is " ) .append  (data.distance).append(" " ).append(data.unit);
+		});
+		
+	}
+});
